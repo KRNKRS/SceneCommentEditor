dce6ac5c94bcb76d89acde1bc02c5891d05aee7f
DESKTOP-S74GNT7\krntr <krntrg_krnkrs@yahoo.co.jp>
2016-12-23 08:06:10 +0900

    シーンビューで後ろを向いてもUIが表示されるのを修正

--- Assets/SceneCommentEditor/Script/CommentDisplay.cs
+++ Assets/SceneCommentEditor/Script/CommentDisplay.cs
@@ -19,6 +19,7 @@ namespace SceneCommentEditor
         private GUIStyle localStyle;
         [SerializeField, HideInInspector]
         private Vector2 buttonSize = new Vector2(100, 19);
+        private bool visible = true;
 
         public void SetManager(CommentManager _manager)
         {
@@ -29,7 +30,7 @@ namespace SceneCommentEditor
 
         void Awake()
         {
-            if(manager == null)
+            if (manager == null)
             {
                 var obj = GameObject.Find("SceneCommentEditor");
                 if (obj == null)
@@ -67,24 +68,44 @@ namespace SceneCommentEditor
         {
             var sceneCamera = SceneView.currentDrawingSceneView.camera;
             var pos = sceneCamera.WorldToScreenPoint(this.transform.position);
-
-            Handles.BeginGUI();
+            var view_pos = sceneCamera.WorldToViewportPoint(this.transform.position);
+            if (view_pos.x < 0.0f || view_pos.x > 1.0f ||
+                view_pos.y < 0.0f || view_pos.y > 1.0f)
             {
-                var buttonStyle = windowButtonSkin.GetStyle("button");
-                var margin = buttonStyle.margin.left * 2;
-                localStyle.fontSize = manager.GetFontSizeByDistance(this.transform.position, sceneCamera.transform.position);
-                var width = localStyle.CalcSize(new GUIContent(text)).x + localStyle.fontSize + margin;
-                var height = localStyle.CalcSize(new GUIContent(text)).y + localStyle.fontSize + margin;
-
-                var buttonRect = new Rect(pos.x, SceneView.currentDrawingSceneView.position.height - pos.y, width, height);
-                if (GUI.Button(buttonRect, "", buttonStyle))
-                {
-                    EditorGUIUtility.PingObject(this.gameObject);
-                }
-                var labelRect = new Rect(buttonRect.x + margin, buttonRect.y, width, height);
-                GUI.Label(labelRect, text, localStyle);
+                visible = false;
             }
-            Handles.EndGUI();
+            else
+            {
+                var from = sceneCamera.transform.TransformDirection(Vector3.forward);
+                var to = this.transform.position - sceneCamera.transform.position;
+                var angle = Vector3.Angle(from, to);
+                if(angle < 90)
+                {
+                    visible = true;
+                }
+            }
+
+            if (visible)
+            {
+
+                Handles.BeginGUI();
+                {
+                    var buttonStyle = windowButtonSkin.GetStyle("button");
+                    var margin = buttonStyle.margin.left * 2;
+                    localStyle.fontSize = manager.GetFontSizeByDistance(this.transform.position, sceneCamera.transform.position);
+                    var width = localStyle.CalcSize(new GUIContent(text)).x + localStyle.fontSize + margin;
+                    var height = localStyle.CalcSize(new GUIContent(text)).y + localStyle.fontSize + margin;
+
+                    var buttonRect = new Rect(pos.x, SceneView.currentDrawingSceneView.position.height - pos.y, width, height);
+                    if (GUI.Button(buttonRect, "", buttonStyle))
+                    {
+                        EditorGUIUtility.PingObject(this.gameObject);
+                    }
+                    var labelRect = new Rect(buttonRect.x + margin, buttonRect.y, width, height);
+                    GUI.Label(labelRect, text, localStyle);
+                }
+                Handles.EndGUI();
+            }
         }
 #endif
     }
